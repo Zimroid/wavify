3532f57a5228af3c01503aa492e4e4dd5786bd38
Zimroid <zimroid@gmail.com>
2021-07-19 22:34:04 +0200

    Remove comment and unexpected style

--- src/SquareCell/SquareCell.tsx
+++ src/SquareCell/SquareCell.tsx
@@ -8,9 +8,8 @@ interface Props {
 }
 
 export default function SquareCell({x, y, children}: Props) {
-// let a:number = "toto";
     return (
-        <div className={styles.cellWrapper} style={{"--x": x, "--y": y, color: "red"}}>
+        <div className={styles.cellWrapper} style={{"--x": x, "--y": y}}>
             <div className={styles.cell}>
                 {children}
             </div>
