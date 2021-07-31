2e303d2bba427d0d3083c37fc811e47124bb8024
Zimroid <zimroid@gmail.com>
2021-07-31 17:18:48 +0200

    Update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('check grid size 1', () => {
+  const { container } = render(<App />);
+  act(() => {
+    global.innerWidth = 500;
+    global.innerHeight = 2000;
+    global.dispatchEvent(new Event('resize'));
+  });
+
+  const grid = container.querySelector('div[style="--nb-columns: 11; --nb-rows: 45;"]');
+
+  expect(grid).toBeInTheDocument();
+});
+
+test('check grid size 2', () => {
+  const { container } = render(<App />);
+  act(() => {
+    global.innerWidth = 2000;
+    global.innerHeight = 500;
+    global.dispatchEvent(new Event('resize'));
+  });
+
+  const grid = container.querySelector('div[style="--nb-columns: 45; --nb-rows: 11;"]');
+
+  expect(grid).toBeInTheDocument();
 });
